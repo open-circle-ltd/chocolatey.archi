3228837d63ecd7daede9d0aa7028198ebb4b692f
kve-occ <210706414+kve-occ@users.noreply.github.com>
2025-05-13 14:29:17 +0200

    Update download URL and version

--- package/tools/chocolateyinstall.ps1
+++ package/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@ $ErrorActionPreference = 'Stop';
 $PackageParameters = Get-PackageParameters
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$urlPackage = 'https://www.archimatetool.com/downloads/archi-5.php?/5.2.0/Archi-Win64-Setup-5.2.0.exe'
-$checksumPackage = 'b8bb4409dcbc70a67cb9fefc77226bb8893957ebdc3f9af07219cbf5fdcaa478c2490ca90941c9a789fa3a4d1488561101e247204a35131dc0b22460edddf20f'
-$urlPackage64 = 'https://www.archimatetool.com/downloads/archi-5.php?/5.2.0/Archi-Win64-Setup-5.2.0.exe'
-$checksumPackage64 = 'b8bb4409dcbc70a67cb9fefc77226bb8893957ebdc3f9af07219cbf5fdcaa478c2490ca90941c9a789fa3a4d1488561101e247204a35131dc0b22460edddf20f'
+$urlPackage = 'https://www.archimatetool.com/downloads/archi/5.6/Archi-Win64-Setup-5.6.0.exe'
+$checksumPackage = '3f59c81ac2b8ba62ce31254b94baf36020813dde91aa02ada4335febcdffc6d83d1b1f34e30a2f838ab419b40533809a993c91630be03535e31d7d0dec7d55e2'
+$urlPackage64 = 'https://www.archimatetool.com/downloads/archi/5.6/Archi-Win64-Setup-5.6.0.exe'
+$checksumPackage64 = '3f59c81ac2b8ba62ce31254b94baf36020813dde91aa02ada4335febcdffc6d83d1b1f34e30a2f838ab419b40533809a993c91630be03535e31d7d0dec7d55e2'
 
 
 $packageArgs = @{
