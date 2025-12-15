49b99efa6ab8c0bcf64ef78b5b87ec46d16c0d42
kve-occ <210706414+kve-occ@users.noreply.github.com>
2025-12-15 16:00:47 +0100

    fix: remove arch url / checksums

--- package/tools/chocolateyinstall.ps1
+++ package/tools/chocolateyinstall.ps1
@@ -4,8 +4,6 @@ $PackageParameters = Get-PackageParameters
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $urlPackage = 'https://www.archimatetool.com/downloads/archi/5.6.0/Archi-Win64-Setup-5.6.0.exe'
 $checksumPackage = '3f59c81ac2b8ba62ce31254b94baf36020813dde91aa02ada4335febcdffc6d83d1b1f34e30a2f838ab419b40533809a993c91630be03535e31d7d0dec7d55e2'
-$urlPackage64 = 'https://www.archimatetool.com/downloads/archi/5.6.0/Archi-Win64-Setup-5.6.0.exe'
-$checksumPackage64 = '3f59c81ac2b8ba62ce31254b94baf36020813dde91aa02ada4335febcdffc6d83d1b1f34e30a2f838ab419b40533809a993c91630be03535e31d7d0dec7d55e2'
 
 
 $packageArgs = @{
@@ -14,9 +12,6 @@ $packageArgs = @{
     url            = $urlPackage
     checksum       = $checksumPackage
     checksumType   = 'SHA512'
-    url64          = $urlPackage64
-    checksum64     = $checksumPackage64
-    checksumType64 = 'SHA512'
     silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
     ValidExitCodes = @(0, 1000, 1101)
 }
